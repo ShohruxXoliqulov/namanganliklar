ce32364324fe4cee59bc5b574f98aa56033212c4
Shohrux <shoxruxxoliqulov314@gmail.com>
2023-06-21 19:40:29 +0500

    second commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function welcome(){
+        return view('welcome');
+    }
+
+    public function article(){
+        return view('pages.article');
+    }
+
+    public function contact(){
+        return view('pages.contact');
+    }
+
+    public function list(){
+        return view('pages.list');
+    }
+}
---
+++ resources/views/sections/categories.blade.php
@@ -0,0 +1,12 @@
+<ul class="navbar__menu basic-flex">
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Узбекистана</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Мир</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Экономика</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Политика</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Общество</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Технологии</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Спорт</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Культура</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Происшествия</a></li>
+    <li class="menu__item"><a href="{{ route('pages.list') }}">Туризм</a></li>
+</ul>
