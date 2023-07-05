608b40fc61133a09e476c0b1ad9664fb3c88f6f2
Shohrux <shoxruxxoliqulov314@gmail.com>
2023-07-05 11:15:05 +0500

    show and edit

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -51,7 +51,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
@@ -62,7 +62,7 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.categories.update', compact('category'));
     }
 
     /**
@@ -74,7 +74,9 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update($request->all());
+
+        return redirect()->route('admin.categories.index')->with('succes', 'Muvaffaqiyatli yangilandi!');
     }
 
     /**
@@ -85,6 +87,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('admin.categories.index')->with('succes', 'Muvaffaqiyatli o`chirildi!');
     }
 }
